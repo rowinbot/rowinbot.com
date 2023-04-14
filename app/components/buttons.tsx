455a3e9607592ddda17b03259dad19ae3ede1be6
Rowin Hernandez <rowinkaleb@gmail.com>
2023-04-14 08:57:22 +0100

    Tweak landing entry buttons

--- app/components/buttons.tsx
+++ app/components/buttons.tsx
@@ -25,7 +25,7 @@ export function JournalEntryButton(props: JournalEntryButtonProps) {
       to={`journal/${props.id}`}
       className="text-sm app-text cursor-pointer space-y-8 group"
     >
-      <div className="aspect-[5/6] rounded-xl group-hover:ring-4 transition-all duration-300 ease-in-out ring-offset-4 ring-offset-white dark:ring-offset-slate-900 ring-[#144fff] overflow-hidden relative group/image">
+      <div className="aspect-[6/7] rounded-xl group-hover:ring-4 transition-all duration-300 ease-in-out ring-offset-4 ring-offset-white dark:ring-offset-slate-900 ring-[#144fff] overflow-hidden relative group/image">
         <BlurrableImage
           className="absolute inset-0 h-full w-full object-cover transition-all ease-in-out group-hover:duration-300 duration-75"
           blurDataUrl={props.entry.imageBlurData}
@@ -37,10 +37,11 @@ export function JournalEntryButton(props: JournalEntryButtonProps) {
       </div>
 
       <div className="space-y-4">
-        <p className="text-2xl">{props.entry.title}</p>
+        <p className="text-xl font-medium">{props.entry.title}</p>
+        <p className="text-base">{props.entry.description}</p>
 
         <p>
-          <span className="font-medium text-xl after:transition-all after:duration-200 after:ease-in-out after:h-1 after:bg-black dark:after:bg-white after:absolute after:left-0 relative after:-bottom-2 after:right-[100%] group-hover:after:right-0">
+          <span className="font-medium text-lg after:transition-all after:duration-200 after:ease-in-out after:h-1 after:bg-black dark:after:bg-white after:absolute after:left-0 relative after:-bottom-2 after:right-[100%] group-hover:after:right-0">
             Read more
           </span>
         </p>
